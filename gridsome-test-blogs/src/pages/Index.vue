2d8fd8333f082d0469e0de15813f70f867c99986
ykaito39 <ykaito39@gmail.com>
2021-07-01 21:24:39 +0900

    add

--- gridsome-test-blogs/src/pages/Index.vue
+++ gridsome-test-blogs/src/pages/Index.vue
@@ -1,33 +1,20 @@
-<template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, Gridsome!</h1>
-
-    <p>
-		はじめてのGridsomeサイト
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
-</template>
-
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, Gridsome!'
+<page-query> // graphQLのクエリを設定する
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+  allPost {
+    totalCount
+    edges {
+      node {
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
   }
 }
-</script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+</page-query>
